00e2ba4dceaeb6e433d92684e8e0d248bd9dd968
Janei <18485105+janeijones@users.noreply.github.com>
2021-04-11 16:52:12 -0500

    Completed questions to write to README file

--- Develop/index.js
+++ Develop/index.js
@@ -4,25 +4,87 @@ const inquirer = require("inquirer")
 
 // TODO: Create an array of questions for user input
 const questions = [ //do this for all 2000000 questions; stack array of objects
-    { type: "input", message: "What is yo mama name?", name: "question1" },
+    {
+        type: "input",
+        message: "Enter the project name:",
+        name: "title"
+    },
+
+    {
+        type: "input",
+        message: "Enter the project description:",
+        name: "description"
+    },
+
+    {
+        type: "input",
+        message: "Enter the steps required for install:",
+        name: "installation"
+    },
+
+    {
+        type: "input",
+        message: "Enter the instructions and examples for use:",
+        name: "usage"
+    },
+
+
+    {
+        type: "input",
+        message: "Enter any collaborator used (including github linkes and third-party assets):",
+        name: "credits"
+    },
+
+
+    {
+        type: "input",
+        message: "Choose license type:",
+        name: "license"
+    },
+
+    {
+        type: "input",
+        message: "Choose badge type:",
+        name: "tile"
+    },
+
+    {
+        type: "input",
+        message: "What is your GitHub ID?",
+        name: "githubId"
+    },
+
+    {
+        type: "input",
+        message: "What is your website URL?",
+        name: "websiteUrl"
+    },
+
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email"
+    },
 ];
 
 // TODO: Create a function to write README file
 inquirer.
     prompt(questions)
-    .then((response) => { 
+    .then((response) => {
 
-  
-    fs.writeFile("README.md",
-        
 
-        `# ${response.question1}` //change here
+        fs.writeFile("README.md",
 
-        , function (error) {
-            if (error) {
-                console.log(error)
-            }
-        })
+
+            `# ${response.title}
+            ${response.usage}
+            ${response.credits}` //generate the README. 
+
+            , function (error) {
+                if (error) {
+                    console.log(error)
+                }
+            })
     })
 
 
